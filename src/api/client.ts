aee62c401fdc7b1037fd3b743c10643b8cd04051
Tom Streller <tom@streller.me>
2021-08-21 20:31:44 +0200

    Add test

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,15 +1,34 @@
-import {
-  ApolloClient,
-  ApolloCache,
-  InMemoryCache,
-  NormalizedCacheObject,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
-export type TApolloCache = ApolloCache<NormalizedCacheObject>;
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080/graphql',
+});
+
+/*
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:8080/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);*/
 
 const client = new ApolloClient({
-  uri: 'http://lcoalhost:8080/graphql',
-  cache: new InMemoryCache()
+  link: httpLink,
+  cache: new InMemoryCache(),
 });
 
 export default client;
